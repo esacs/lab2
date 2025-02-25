984b85c68baeccda7c159f0c87b62167e1bb5921
Elit <elit_hanna@hotmail.com>
2025-02-24 22:19:52 -0600

    Lab2 Commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import { SimpleForm } from './components/SimpleForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SimpleForm></SimpleForm>
+    <AppCounter></AppCounter>
   </StrictMode>,
 )
